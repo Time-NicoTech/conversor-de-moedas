9c43e260ea818fed6c2c8a0c967d98e6ce837bb8
isaac451-andrade <166961938+isaac451-andrade@users.noreply.github.com>
2025-10-24 21:59:31 -0300

    feat(scripts): adicionado inicio do evento de converter moeda

--- scripts.js
+++ scripts.js
@@ -1,7 +1,21 @@
-const valoresConversao = {
-    doReal:{
-        dolar:0.185,
-        euro:0.159,
-        libra:0.139
+document.querySelector(".converter-btn").addEventListener("click", ()=>{
+    const valorInput = document.querySelector(".input-valor").value;
+
+    const btnAtivo = document.querySelector(".btn.active");
+    
+    const resultado = parseFloat(valorInput) * parseFloat(btnAtivoValor.dataset.valor);
+
+    const simboloMap = {
+        dolar: "$",
+        euro: "€",
+        libra: "£"
     }
+
+    const display = document.querySelector(".display-valor");
+    display.textContent = ``;
+
+})
+
+const calcularValor = (valor)=>{
+    
 }
