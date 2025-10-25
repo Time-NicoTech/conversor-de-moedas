9a638fcf3362b032ca167a1fd6b9982e99980f5b
isaac451-andrade <166961938+isaac451-andrade@users.noreply.github.com>
2025-10-24 22:11:58 -0300

    feat(scripts): adicionado formato das moedas ao valor

--- scripts.js
+++ scripts.js
@@ -3,16 +3,22 @@ document.querySelector(".converter-btn").addEventListener("click", ()=>{
 
     const btnAtivo = document.querySelector(".btn.active");
     
-    const resultado = parseFloat(valorInput) * parseFloat(btnAtivoValor.dataset.valor);
+    const resultado = (parseFloat(valorInput) * parseFloat(btnAtivoValor.dataset.valor)).toLocaleString(
+        'pt-br',
+        {
+            style:"currency",
+            currency:"BRL"
+        }
+    );
 
     const simboloMap = {
         dolar: "$",
         euro: "€",
         libra: "£"
     }
-
+    
     const display = document.querySelector(".display-valor");
-    display.textContent = ``;
+    display.textContent = `${simboloMap[btnAtivo.getAttribute("name")]} ${resultado}`;
 
 })
 
